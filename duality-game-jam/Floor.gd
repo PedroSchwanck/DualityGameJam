9ba41857bf5ed619ec7b7ca638148489be8dc20f
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-10-18 16:16:44 -0300

    removed the main file

--- duality-game-jam/Floor.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-
-func move_horizontally(distance):
-	var textureWidth = texture.get_size().x
-	
-	global_position.x -= floor(distance)
-
-	if (global_position.x + textureWidth) <= 0:
-		global_position.x = textureWidth
