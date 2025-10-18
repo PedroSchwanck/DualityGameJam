c0f2c0387c6a3155acb4038aa744a55b7c1be124
EduardoAlcaria <eduardoalcarialopes@gmail.com>
2025-10-18 04:42:48 -0300

    added the dino files

---
+++ duality-game-jam/Cactus.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+signal exitedScreen
+
+var speed
+
+var randomNumberGenerator = RandomNumberGenerator.new()
+
+func _init():
+	add_to_group("Cacti")
+
+func _ready():
+	randomNumberGenerator.randomize()
+	
+	$Sprite2D.set_frame(randomNumberGenerator.randi_range(0, 5))
+
+func _process(delta):
+	global_position.x -= floor(speed * delta)
+	
+func set_velocity(distance):
+	speed = distance
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	emit_signal("exitedScreen")
+	queue_free()
---
+++ duality-game-jam/Floor.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+func move_horizontally(distance):
+	var textureWidth = texture.get_size().x
+	
+	global_position.x -= floor(distance)
+
+	if (global_position.x + textureWidth) <= 0:
+		global_position.x = textureWidth
